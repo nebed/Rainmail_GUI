4e2e65f4cba18adf5fff2440567162b42b435621
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-25 13:40:07 +0100

    Update Rainmail_GUI.py

--- Rainmail_GUI.py
+++ Rainmail_GUI.py
@@ -1,10 +1,19 @@
 #-*-coding:utf8;-*-
 from tkinter import *
+
+def choose_keyword():
+	
 	
 def express_gui():
 	express = Toplevel(rainmail)
 	express.title('Express Mode')
 	express.geometry('500x400')
+	values_to_set = Menubutton(express, text='Values to Set', relief=RAISED)
+	values_to_set.grid()
+	values_to_set_menu = Menu(values_to_set, tearoff=0)
+	values_to_set_menu.add_command = (label='Choose What to Search', command=choose_keyword())
+	
+	
     
 
 def prompt_gui():
@@ -17,9 +26,9 @@ def prompt_gui():
 	menubar.add_cascade(label='File', menu=filemenu)
     
 	options = Menu(menubar, tearoff=0)
-	options.add_command(label='Express Mode', command=express_gui)
+	options.add_command(label='Express Mode', command=express_gui())
 	options.add_seperator()
-	options.add_command(label='Normal Mode', command=user_gui)
+	options.add_command(label='Normal Mode', command=user_gui())
 	menubar.add_cascade(label='Select Mode', menu=options)
     
     
