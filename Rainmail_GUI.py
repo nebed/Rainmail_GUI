8283ebefcebf5d7987e2afa26554775eeb86e023
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-25 13:20:52 +0100

    Add files via upload

---
+++ Rainmail_GUI.py
@@ -0,0 +1,31 @@
+#-*-coding:utf8;-*-
+from tkinter import *
+	
+def express_gui():
+
+    express = Toplevel(rainmail)
+    express.title('Express Mode')
+    express.geometry('500x400')
+    
+    
+
+def prompt_gui():
+
+    rainmail = Tk()
+    rainmail.title('Rainmail GUI')
+    rainmail.geometry('500x400')
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label='Exit', command=rainmail.quit)
+    menubar.add_cascade(label='File', menu=filemenu)
+    
+    options = Menu(menubar, tearoff=0)
+    options.add_command(label='Express Mode', command=express_gui)
+    options.add_seperator()
+    options.add_command(label='Normal Mode', command=user_gui)
+    menubar.add_cascade(label='Select Mode', menu=options)
+    
+    
+    
+    
+    
