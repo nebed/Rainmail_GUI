4523206fcb8d6878af9e1b730e11af1a450719b1
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-25 20:12:01 +0100

    Update Rainmail_GUI.py

--- Rainmail_GUI.py
+++ Rainmail_GUI.py
@@ -318,10 +318,71 @@ def express_gui():
 		'google_sleeping_ranges':google_sleeping_ranges_default
 
 		}
+	
+	
 
 	return config
 	
+def start_search(config):
+	urls_to_process = deque()
+	try:
+		search = scrape_with_config(config)
+	except GoogleSearchError as e:
+		print(e)
+
+	for serp in search.serps:
+		print(serp)
+
+		for link in serp.links:
+			print(link)
+			retrieved_url = str(link)
+			end = len(retrieved_url) -1
+			urls_to_process.append(retrieved_url[25:end])
+	return urls_to_process
+
+#funtion to extract email addresses from web pages
+
+def process_urls(urls_to_process):
+
+	processed_urls = set()  # a set of urls that have already been crawled
+	emails = set()  # a set of extracted emails
+
+	while len(urls_to_process) > 0:  # process urls one by one until we exhaust the queue
+
+		url = urls_to_process.popleft()  # move next url from the queue to the set of processed urls
+		processed_urls.add(url)
+		parts = urlsplit(url)    # extract base url to resolve relative links
+		base_url = "{0.scheme}://{0.netloc}".format(parts)
+		path = url[:url.rfind('/')+1] if '/' in parts.path else url
+		print("Processing %s" % url)
+		try:
+			response = requests.get(url)
+		except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+		# ignore pages with errors
+			continue
+
+		new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))   # extract all email addresses and add them into a set
+		emails.update(new_emails)
+		soup = BeautifulSoup(response.text)     # create a beutiful soup for the html document
+
+		for anchor in soup.find_all("a"):
+			link = anchor.attrs["href"] if "href" in anchor.attrs else ''  # extract link url from the anchor
+
+			if link.startswith('/'):   # resolve relative links
+				link = base_url + link
+
+			elif not link.startswith('http'):
+				link = path + link
+
+			if not link in urls_to_process and not link in processed_urls:   # add the new url to the queue if it was not enqueued nor processed yet
+				urls_to_process.append(link)
+	f = open('Rainmails.txt','w')    #indicate name of output file to write emails
+	#sys.stdout = f
+	#path= '/home/Desktop'  #for linux
+	f.write(emails) 
+	f.close()
 	
+	return emails
     
 
 def prompt_gui():
@@ -332,7 +393,6 @@ def prompt_gui():
 	filemenu = Menu(menubar, tearoff=0)
 	filemenu.add_command(label='Exit', command=rainmail.quit)
 	menubar.add_cascade(label='File', menu=filemenu)
-    
 	options = Menu(menubar, tearoff=0)
 	options.add_command(label='Express Mode', command=express_gui)
 	options.add_seperator()
