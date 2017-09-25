07825fa28557f16062f87e2ee23d4d2fa05214a6
Uchenna Nebedum <nebeduch@gmail.com>
2017-09-25 13:30:51 +0100

    Update Rainmail_GUI.py

--- Rainmail_GUI.py
+++ Rainmail_GUI.py
@@ -2,28 +2,25 @@
 from tkinter import *
 	
 def express_gui():
-
-    express = Toplevel(rainmail)
-    express.title('Express Mode')
-    express.geometry('500x400')
-    
+	express = Toplevel(rainmail)
+	express.title('Express Mode')
+	express.geometry('500x400')
     
 
 def prompt_gui():
-
-    rainmail = Tk()
-    rainmail.title('Rainmail GUI')
-    rainmail.geometry('500x400')
-    menubar = Menu(root)
-    filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label='Exit', command=rainmail.quit)
-    menubar.add_cascade(label='File', menu=filemenu)
+	rainmail = Tk()
+	rainmail.title('Rainmail GUI')
+	rainmail.geometry('500x400')
+	menubar = Menu(root)
+	filemenu = Menu(menubar, tearoff=0)
+	filemenu.add_command(label='Exit', command=rainmail.quit)
+	menubar.add_cascade(label='File', menu=filemenu)
     
-    options = Menu(menubar, tearoff=0)
-    options.add_command(label='Express Mode', command=express_gui)
-    options.add_seperator()
-    options.add_command(label='Normal Mode', command=user_gui)
-    menubar.add_cascade(label='Select Mode', menu=options)
+	options = Menu(menubar, tearoff=0)
+	options.add_command(label='Express Mode', command=express_gui)
+	options.add_seperator()
+	options.add_command(label='Normal Mode', command=user_gui)
+	menubar.add_cascade(label='Select Mode', menu=options)
     
     
     
